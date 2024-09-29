9c9844a15dc46e729f6a0966ee922b5503fbe537
Prayoga K. I. <113886602+Pk2413@users.noreply.github.com>
2024-09-29 17:52:41 +0700

    update firebase

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   colorScheme: ColorScheme.light(
-    background: Colors.grey.shade300,
-    primary: Colors.grey.shade500,
-    secondary: Colors.grey.shade200,
+    background: Colors.white,
+    primary: Color.fromARGB(255, 54, 124, 245),
     tertiary: Colors.white,
     inversePrimary: Colors.grey.shade900,
   ),
